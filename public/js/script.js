99df5ddcde4d58fa59e358caa9ec7b596f1429c6
Alejandro <alegaralonrgmail.com>
2023-07-12 17:21:50 +0200

    prueba

---
+++ public/js/script.js
@@ -0,0 +1,4 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("library-project[201~ JS imported successfully!");
+});
