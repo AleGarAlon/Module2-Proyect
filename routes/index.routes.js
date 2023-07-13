0df2b3c5d72b22af45bf6ee27662644c3b16bfbe
Alejandro <alegaralonrgmail.com>
2023-07-13 14:13:30 +0200

    auth-Login-Signup-Session-middleware

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const { isLoggedIn, isAdmin } = require('../middlewares/route-guard.middleware')
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
 
+/* GET profile page */
+router.get("/profile", isLoggedIn, (req, res, next) => {
+  console.log(req.session.user)
+  res.render("profile", { user: req.session.user });
+});
 module.exports = router;
