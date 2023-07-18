dbce25cfb749ee9ffe8724a5c0112f9f5cca3168
Alejandro <alegaralonrgmail.com>
2023-07-18 12:22:53 +0200

    updates&deletes

--- routes/kids.routes.js
+++ routes/kids.routes.js
@@ -14,20 +14,15 @@ router.get('/newKid' ,(request ,  response) =>{
 
 router.post('/newKid', async(req , res) => {
         const kidInfo = req.body
-        console.log(`The req body is :`,req.body)
         const parentInfo = req.session.user
         kidInfo.parentId = parentInfo._id
-        console.log(kidInfo)
     try{
         const activities = await Activity.find({ age: { $in: kidInfo.age } });
         kidInfo.activities = activities.map(activity => activity._id);
-       
-        console.log(kidInfo);
         const newKid = await Kid.create(kidInfo)
         const parent = await User.findByIdAndUpdate(parentInfo._id, 
             { $push: { kids: newKid } }, 
-            { new: true });
-        console.log(parent)    
+            { new: true });    
         res.redirect("/profile")
    } 
    catch (error){
@@ -39,15 +34,52 @@ router.post('/newKid', async(req , res) => {
 router.get('/:kidId', async (req , res) =>{
     try{
         const kidId = req.params.kidId
-        console.log(kidId)
         const kidDetails = await Kid.findById(kidId).populate("activities")
-        console.log(kidDetails)
         res.render("kids/kid-info", {kid : kidDetails})
     }
     catch (error){
         console.log(error)
     }
 })
+
+router.get('/:kidId/update', async (req , res) =>{
+    try{
+        const kidId = req.params.kidId
+        const kidDetails = await Kid.findById(kidId).populate("activities")
+        res.render("kids/kid-update", {kid : kidDetails})
+    }
+    catch (error){
+        console.log(error)
+    }
+})
+
+router.post('/:kidId/update', async(req , res) => {
+    const kidInfo = req.body
+    const kidId = req.params.kidId
+try{
+    const activities = await Activity.find({ age: { $in: kidInfo.age } });
+    kidInfo.activities = activities.map(activity => activity._id);
+    const updatedKid = await Kid.findByIdAndUpdate(kidId, kidInfo, { new: true })
+    
+    res.redirect("/profile")
+} 
+catch (error){
+console.log(error)
+}
+
+})
+
+router.post("/:kidId/delete", async (req, res, next) => {
+    const kidId = req.params.kidId
+    console.log(kidId)
+  try{
+    const deletedKid = await Kid.findByIdAndDelete(kidId)
+    res.redirect("/profile")
+  } 
+  catch (error){
+  console.log(error)
+  }
+  })
 
 
 module.exports = router;
