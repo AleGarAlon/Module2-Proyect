279dc6a1f2ccb927c831ca185a6df8dd9da2f128
Jaime Toro <zomo505@gmail.com>
2023-07-21 11:02:37 +0200

    more interface tweaks

--- public/js/script.js
+++ public/js/script.js
@@ -1,27 +1,12 @@
-
-document.addEventListener('DOMContentLoaded', function() {
-  let elems = document.querySelectorAll('.parallax');
-  let instances = M.Parallax.init(elems, {
-    // specify options here
-  });
-});
-
-  
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.carousel');
   var instances = M.Carousel.init(elems, {
     fullWidth: true,
     indicators: true,
     dist: 0,
+    duration: 500,
   });
-});
-        
-
-
-let instance = M.Carousel.init({
-  fullWidth: true
-});
-        
+});       
 
 
 M.AutoInit();
